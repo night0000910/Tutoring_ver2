aeee18fa64c831e4b8a7b01572682a8db3e27be3
night0000910 <night0000910@gmail.com>
2021-12-28 16:34:08 +0900

    conceal SECRET_KEY in settings.py

--- tutoring/settings.py
+++ tutoring/settings.py
@@ -10,9 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
 import os
 
+
+env = environ.Env()
+env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-43zk)9rfd2ura5ieay4s&$lgfovzqbw+*&20b=!3sjx_9_eom)'
+#SECRET_KEY = 'django-insecure-43zk)9rfd2ura5ieay4s&$lgfovzqbw+*&20b=!3sjx_9_eom)'
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
